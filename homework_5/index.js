8b7dda4138849058d0400fa59732b86104bc6f69
VladlenGorbunov <gorbunov.vladlen2014@gmail.com>
2021-10-07 16:09:12 +0300

    Сделана задача 2

--- homework_5/index.js
+++ homework_5/index.js
@@ -14,3 +14,19 @@ function getNonUniqueArrValue(arr) {
     return [...new Set(arr)];
 }
 
+/************
+ * Задача 2 *
+ ************/
+
+taskTwo();
+
+function taskTwo() {
+    let arr = [1, 3, 5, 4, 3, 2, 4];
+    console.log("Результат задачи 2:");
+    console.table(reverseArr(arr));
+}
+
+function reverseArr(arr) {
+    return arr.map((item, index, curArr) => curArr[curArr.length - index - 1]);
+}
+
