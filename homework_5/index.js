e8bd4db6a285d2eda590b07f4ddac6bba9c0a350
VladlenGorbunov <gorbunov.vladlen2014@gmail.com>
2021-10-07 16:54:11 +0300

    Сделана задача 4

--- homework_5/index.js
+++ homework_5/index.js
@@ -6,8 +6,9 @@ taskOne();
 
 function taskOne() {
     let arr = [1, 3, 5, 4, 3, 2, 4, 5, -2, "string", 5, "string", "apple", -1, -2];
-    console.log("Результат задачи 1:");
+    console.group("Результат задачи 1:");
     console.table(getNonUniqueArrValue(arr));
+    console.groupEnd();
 }
 
 function getNonUniqueArrValue(arr) {
@@ -21,13 +22,20 @@ function getNonUniqueArrValue(arr) {
 taskTwo();
 
 function taskTwo() {
-    let arr = [1, 3, 5, 4, 3, 2, 4];
-    console.log("Результат задачи 2:");
+    let arr = [1, 3, 5, 4, 3, 2, 4, "0"];
+    console.group("Результат задачи 2:");
     console.table(reverseArr(arr));
+    console.groupEnd();
 }
 
 function reverseArr(arr) {
     return arr.map((item, index, curArr) => curArr[curArr.length - index - 1]);
+    /* Доп. вариант
+    return arr.reduceRight((prev, curr) => {
+        prev.push(curr);
+        return prev;
+    }, []);
+    */
 }
 
 /************
@@ -42,8 +50,9 @@ function taskThree() {
         ["age", 35],
         [0, "value"]
     ];
-    console.log("Результат задачи 3:");
+    console.group("Результат задачи 3:");
     console.log(arrMapToObjMap(arrMap));
+    console.groupEnd();
 }
 
 function arrMapToObjMap(arrMap) {
@@ -53,4 +62,38 @@ function arrMapToObjMap(arrMap) {
     });
     return resultObj;
     //return Object.fromEntries(new Map(arrMap)); //доп.вариант
+}
+
+/************
+ * Задача 4 *
+ ************/
+
+taskFour();
+
+function taskFour() {
+    let obj = {
+        name: "floor",
+        height: 50,
+        width: 100,
+        getArea: () => this.height * this.width,
+        color: "#909090",
+        0: {
+            test: "text"
+        },
+        isSquare: this.height === this.width
+    };
+
+    console.group("Результат задачи 4:");
+    console.log(getSumNumericValuesObject(obj));
+    console.groupEnd();
+}
+
+function getSumNumericValuesObject(obj) {
+    let resultSum = 0;
+    for (const key in obj) {
+        if (typeof obj[key] == "number") {
+            resultSum += obj[key];
+        }
+    }
+    return resultSum;
 }
