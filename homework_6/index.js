ce3d7d6f6f6fecc27a7a9cf30f9ffa5920fa7657
VladlenGorbunov <gorbunov.vladlen2014@gmail.com>
2021-10-09 22:26:22 +0300

    Сделана задача 1
    * Написаны две рекурсивные функции (быстрая и медленная)

---
+++ homework_6/index.js
@@ -0,0 +1,30 @@
+/************
+ * Задача 1 *
+ ************/
+
+taskOne();
+
+function taskOne() {
+    console.log("Результат задачи 1 (fast): " + getNumFibonacciFast(40));
+    console.log("Результат задачи 1 (slow): " + getNumFibonacciSlow(40));
+}
+
+
+function getNumFibonacciFast(index) {
+    //Линейная рекурсия, возвращаются 2 соседних числа
+    const fibonacciFastRecursion = index => {
+        if (index === 0) {
+            return [0, 1];
+        }
+
+        let [prevVal, nextVal] = fibonacciFastRecursion(index - 1);
+        return [nextVal, prevVal + nextVal];
+    };
+
+    return fibonacciFastRecursion(index)[0];
+}
+
+//Медленная рекурсивная функция, поскольку получается дерево вложенностей из-за двух рекурсивных вызовов
+function getNumFibonacciSlow(index) {
+    return index > 1 ? getNumFibonacciSlow(index - 1) + getNumFibonacciSlow(index - 2) : index;
+}
