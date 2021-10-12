b892014d19e84192d5eef83b9fe6d99daa0d2588
VladlenGorbunov <gorbunov.vladlen2014@gmail.com>
2021-10-12 17:36:21 +0300

    Задача сделана на функциях-конструкторах

--- homework_7/index.js
+++ homework_7/index.js
@@ -22,6 +22,7 @@ const animalObj = {
 
 Object.defineProperties(animalObj, {
     "_name": {configurable: false},
+    "name": {configurable: false, enumerable: false},
     "eat": {configurable: false, writable: false, enumerable: false},
     "say": {configurable: false, writable: false, enumerable: false},
     "rename": {configurable: false, writable: false, enumerable: false}
@@ -47,9 +48,7 @@ const dogObj = {
     }
 };
 
-Object.defineProperties(dogObj, {
-    "say": {configurable: false, writable: false, enumerable: false}
-});
+Object.defineProperty(dogObj, "say", {configurable: false, writable: false, enumerable: false});
 
 const parrotObj = {
     say() {
@@ -57,9 +56,7 @@ const parrotObj = {
     }
 };
 
-Object.defineProperties(parrotObj, {
-    "say": {configurable: false, writable: false, enumerable: false}
-});
+Object.defineProperty(parrotObj, "say", {configurable: false, writable: false, enumerable: false});
 
 Object.setPrototypeOf(catObj, animalObj);
 Object.setPrototypeOf(dogObj, animalObj);
@@ -92,6 +89,86 @@ console.groupEnd();
  * Функции-конструкторы *
  ************************/
 
+function AnimalFunc(name) {
+    this._name = name;
+    this.eat = function () {
+        console.log(`${this._name} ест`);
+    }
+
+    this.rename = function (name) {
+        this._name = name;
+    }
+
+    this._say = function () {
+        console.log("Неизвестное животное молчит")
+    }
+
+    Object.defineProperties(this, {
+        "_name": {configurable: false},
+        "name": {
+            get() {
+                return this._name
+            }, configurable: false, enumerable: false
+        },
+        "_say": {configurable: false, enumerable: false},
+        "say": {
+            value: function () {
+                this._say();
+            },
+            configurable: false, enumerable: false, writable: false
+        },
+        "eat": {configurable: false, writable: false, enumerable: false},
+        "rename": {configurable: false, writable: false, enumerable: false}
+    });
+}
+
+function CatFunc(name) {
+    AnimalFunc.call(this, name);
+    this.hunt = function () {
+        console.log(`${this.name} охотится`);
+    }
+    this._say = function () {
+        console.log("Кот молчит")
+    }
+    Object.defineProperty(this, "hunt", {configurable: false, writable: false, enumerable: false});
+}
+
+function DogFunc(name) {
+    AnimalFunc.call(this, name);
+    this._say = function () {
+        console.log("Собака молчит")
+    }
+}
+
+function ParrotFunc(name) {
+    AnimalFunc.call(this, name);
+    this._say = function () {
+        console.log("Попугай молчит")
+    }
+}
+
+const animalF = new AnimalFunc("Животное"),
+    catF = new CatFunc("Снежок"),
+    dogF = new DogFunc("Шарик"),
+    parrotF = new ParrotFunc("Кеша");
+
+console.group("Результат с функциями-конструкторами:");
+animalF.eat();
+animalF.say();
+console.log("---------------");
+
+catF.eat();
+catF.hunt();
+catF.say();
+console.log("---------------");
+
+dogF.eat();
+dogF.say();
+console.log("---------------");
+
+parrotF.eat();
+parrotF.say();
+console.groupEnd();
 
 /**********
  * Классы *
