5353a29b56aac402d313f7b5d7227d6001f8e215
VladlenGorbunov <gorbunov.vladlen2014@gmail.com>
2021-10-25 21:08:28 +0300

    Сделан компонент Card

--- homework_13/src/components/app/App.jsx
+++ homework_13/src/components/app/App.jsx
@@ -6,6 +6,7 @@ import Sidebar from "../sidebar/Sidebar";
 import Main from "../main/Main";
 import AppWrapper from "./app-wrapper/AppWrapper";
 import Section from "../section/Section";
+import Card from "../card/Card";
 
 class App extends React.Component {
   render() {
@@ -16,10 +17,16 @@ class App extends React.Component {
           <Sidebar/>
           <Main>
             <Section titleH1="Рыба на любой вкус" subtitle="Мы продаем рыбу, а не только показываем!">
-
+              <Card link="#" linkText="Замороженная рыба" description="Мы заморозили рыбу для вас"/>
+              <Card link="#" linkText="Живая рыба" description="На кухню или на разведение"/>
             </Section>
             <Section titleH2="Популярные">
-
+              <Card imgSrc="#" imgAlt="img" link="#" linkText="Палтус" buttonText="Купить"/>
+              <Card imgSrc="#" imgAlt="img" link="#" linkText="Сёмга" buttonText="Купить"/>
+              <Card imgSrc="#" imgAlt="img" link="#" linkText="Сом" buttonText="Купить"/>
+              <Card imgSrc="#" imgAlt="img" link="#" linkText="Мойва" buttonText="Купить"/>
+              <Card imgSrc="#" imgAlt="img" link="#" linkText="Сельдь" buttonText="Купить"/>
+              <Card imgSrc="#" imgAlt="img" link="#" linkText="Тунец" buttonText="Купить"/>
             </Section>
           </Main>
           <Footer info="Контакты" copy="@ 2021. ИП Рыбаков О. А."/>
