b4b090914cd6e0da93bf212e295f75ba71b15760
VladlenGorbunov <gorbunov.vladlen2014@gmail.com>
2021-12-07 02:15:33 +0300

    Рефакторинг клиентского приложения, переписаны функции-обработчики в одну функцию. Выводятся сообщения об ошибках. При отправке запроса поле и кнопка неактивны

--- homework_31/client/index.js
+++ homework_31/client/index.js
@@ -1,40 +1,51 @@
 const btn = document.getElementById('button');
 const fieldText = document.getElementById('fieldText');
 const outputText = document.getElementById('outputText');
+const outputError = document.getElementById('outputError');
 
 const BASE_URL = 'http://127.0.0.1:3000/api';
 const API_POINT_TEXT = '/text';
 
-const handleLoadDocument = async () => {
-  const response = await fetch(BASE_URL + API_POINT_TEXT);
-  const json = await response.json();
+const optionsPostRequest = {
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  },
+  method: 'POST'
+};
+
+const renderAlerts = (response, json) => {
   if (response.ok) {
+    outputText.style.display = 'block';
     outputText.innerText = json.data.text;
+    outputError.style.display = 'none';
   } else {
-    outputText.innerText = `Error ${json.status}: ${json.message}`;
+    outputError.style.display = 'block';
+    outputError.innerText = `Error ${json.status}: ${json.message}`;
+    outputText.style.display = 'none';
   }
 };
 
-const handleClickButtonForSendTest = async () => {
-  if (fieldText.value) {
-    const response = await fetch(BASE_URL + API_POINT_TEXT, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      method: 'POST',
-      body: JSON.stringify({ text: fieldText.value })
-    });
-    const json = await response.json();
-
-    if (response.ok) {
-      outputText.innerText = json.data.text;
-    } else {
-      outputText.innerText = `Error ${json.status}: ${json.message}`;
+const sendRequest = async (options = undefined) => {
+  fieldText.setAttribute('disabled', 'disabled');
+  btn.setAttribute('disabled', 'disabled');
+  let response = '';
+  if (options !== undefined) {
+    if (fieldText.value && fieldText.value !== outputText.innerText) {
+      response = await fetch(BASE_URL + API_POINT_TEXT, {
+        ...options,
+        body: JSON.stringify({ text: fieldText.value })
+      });
+      fieldText.value = '';
     }
-    fieldText.value = '';
+  } else {
+    response = await fetch(BASE_URL + API_POINT_TEXT);
   }
+  const json = await response.json();
+  renderAlerts(response, json);
+  fieldText.removeAttribute('disabled');
+  btn.removeAttribute('disabled');
 };
 
-window.addEventListener('load', handleLoadDocument);
+window.addEventListener('load', () => sendRequest());
 
-btn.addEventListener('click', handleClickButtonForSendTest);
+btn.addEventListener('click', () => sendRequest(optionsPostRequest));
