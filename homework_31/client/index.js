f9f420be6697608eb8169cb87ef032f501264021
VladlenGorbunov <gorbunov.vladlen2014@gmail.com>
2021-12-07 02:31:44 +0300

    Исправлен вывод текста по условию

--- homework_31/client/index.js
+++ homework_31/client/index.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById('button');
 const fieldText = document.getElementById('fieldText');
-const outputText = document.getElementById('outputText');
 const outputError = document.getElementById('outputError');
 
 const BASE_URL = 'http://127.0.0.1:3000/api';
@@ -13,15 +12,13 @@ const optionsPostRequest = {
   method: 'POST'
 };
 
-const renderAlerts = (response, json) => {
+const renderField = (response, json) => {
   if (response.ok) {
-    outputText.style.display = 'block';
-    outputText.innerText = json.data.text;
+    fieldText.value = json.data.text;
     outputError.style.display = 'none';
   } else {
     outputError.style.display = 'block';
     outputError.innerText = `Error ${json.status}: ${json.message}`;
-    outputText.style.display = 'none';
   }
 };
 
@@ -30,18 +27,17 @@ const sendRequest = async (options = undefined) => {
   btn.setAttribute('disabled', 'disabled');
   let response = '';
   if (options !== undefined) {
-    if (fieldText.value && fieldText.value !== outputText.innerText) {
+    if (fieldText.value) {
       response = await fetch(BASE_URL + API_POINT_TEXT, {
         ...options,
         body: JSON.stringify({ text: fieldText.value })
       });
-      fieldText.value = '';
     }
   } else {
     response = await fetch(BASE_URL + API_POINT_TEXT);
   }
   const json = await response.json();
-  renderAlerts(response, json);
+  renderField(response, json);
   fieldText.removeAttribute('disabled');
   btn.removeAttribute('disabled');
 };
